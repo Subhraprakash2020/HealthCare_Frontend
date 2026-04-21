b3b6bfa76c565dddcb9e4ba721ad0e7476ccb9ea
Subhraprakash2020 <subhra.techy@gmail.com>
2026-04-21 20:11:44 +0530

    create-homepage-layout
      - Update signin and signup button style.

--- src/components/patients/home/Header.jsx
+++ src/components/patients/home/Header.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { Button, Container, Nav, Navbar } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+  const hideHomeMenuLinks =
+    location.pathname === "/patient/signin" ||
+    location.pathname === "/patient/signup";
+  const signInClassName =
+    location.pathname === "/patient/signin" ? "nav-btn-solid" : "nav-btn-outline";
+  const signUpClassName =
+    location.pathname === "/patient/signup" ? "nav-btn-solid" : "nav-btn-outline";
+
   return (
     <Navbar expand="lg" className="healthcare-navbar sticky-top">
       <Container>
@@ -19,17 +28,19 @@ function Header() {
         <Navbar.Toggle aria-controls="home-navbar-nav" />
 
         <Navbar.Collapse id="home-navbar-nav">
-          <Nav className="mx-auto healthcare-nav-links">
-            <Nav.Link href="#services">Services</Nav.Link>
-            <Nav.Link href="#doctors">Doctors</Nav.Link>
-            <Nav.Link href="#testimonials">Testimonials</Nav.Link>
-          </Nav>
+          {!hideHomeMenuLinks && (
+            <Nav className="mx-auto healthcare-nav-links">
+              <Nav.Link href="#services">Services</Nav.Link>
+              <Nav.Link href="#doctors">Doctors</Nav.Link>
+              <Nav.Link href="#testimonials">Testimonials</Nav.Link>
+            </Nav>
+          )}
 
-          <div className="healthcare-nav-actions">
-            <Button as={Link} to="/patient/signin" className="nav-btn-outline">
+          <div className="healthcare-nav-actions ms-auto">
+            <Button as={Link} to="/patient/signin" className={signInClassName}>
               Sign In
             </Button>
-            <Button as={Link} to="/patient/signup" className="nav-btn-solid">
+            <Button as={Link} to="/patient/signup" className={signUpClassName}>
               Sign Up
             </Button>
           </div>
